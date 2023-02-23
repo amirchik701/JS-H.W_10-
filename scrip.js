298b3f5b804187d06f24487408894f0c58b44395
amirchik701 <121165439+amirchik701@users.noreply.github.com>
2023-02-23 21:11:08 +0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,38 +1,38 @@
-// let table = document.querySelector('.table');
+let table = document.querySelector('.table');
 
-// let amountBoxes = 621;
-
-
-// for (let i = 0; i < amountBoxes; i++) {
-//     let box = document.createElement('div')
-//     box.classList.add('box');
-
-//     table.append(box)
-
-//     box.addEventListener('mouseenter', () => setColor(box))
-//     box.addEventListener('mouseleave', () => removeColor(box))
-
-// }
-
-// let arr = ['pink', 'green', 'purple', 'yellow', 'red', 'blue']
+let amountBoxes = 621;
 
 
-// function setColor(item) {
-//     item.style = `background: rgb(${randColor()}, ${randColor()}, ${randColor()});`
-//     // item.style = `background: green;`
-// }
+for (let i = 0; i < amountBoxes; i++) {
+    let box = document.createElement('div')
+    box.classList.add('box');
+
+    table.append(box)
+
+    box.addEventListener('mouseenter', () => setColor(box))
+    box.addEventListener('mouseleave', () => removeColor(box))
+
+}
+
+let arr = ['pink', 'green', 'purple', 'yellow', 'red', 'blue']
 
 
-// function removeColor(item) {
-//     item.style = `background: '';`
-// }
+function setColor(item) {
+    item.style = `background: rgb(${randColor()}, ${randColor()}, ${randColor()});`
+    // item.style = `background: green;`
+}
 
 
-// function randColor() {
-//   // Получаю красный, зеленый и синий компоненты из координат мыши
-//   let randomNumber = Math.floor(Math.random() * 255 + 1) ;
-//   return randomNumber
-// }
+function removeColor(item) {
+    item.style = `background: '';`
+}
+
+
+function randColor() {
+  // Получаю красный, зеленый и синий компоненты из координат мыши
+  let randomNumber = Math.floor(Math.random() * 255 + 1) ;
+  return randomNumber
+}
 
 /* innerWidth возвращает внутреннюю ширину окна в пикселях. Это включает в себя ширину вертикальной полосы прокрутки, если она присутствует. */
 /* Math.floor() метод всегда округляет в меньшую сторону и возвращает наибольшее целое число, меньшее или равное заданному числу. */
@@ -119,9 +119,9 @@
 // console.log(arr);
 
 
-let arr = [1, 2, 3, 4, 5]
-let del = arr.splice(0, 'a',)
-console.log(del);
+// let arr = [1, 2, 3, 4, 5]
+// let del = arr.splice(0, 'a',)
+// console.log(del);
 
 
 
